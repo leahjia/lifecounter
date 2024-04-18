4de281acd8471558b3388139d257c08ce45a2037
Leah Jia <99011487+leahjia@users.noreply.github.com>
2024-04-17 22:47:39 -0700

    iii

--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -10,92 +10,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var loser: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
     }
-
-    @IBAction func player1plus1(_ sender: Any) {
-        print("player1plus1")
-        if let lifeText = label1.text {
-            let components = lifeText.components(separatedBy: ":")
-            if components.count == 2, let currentLife = Int(components[1].trimmingCharacters(in: .whitespaces)) {
-                let newLife = currentLife + 1
-                label1.text = "Player 1 life: \(newLife)"
-            }
-        }
-    }
-    @IBAction func player1plus5(_ sender: Any) {
-        print("player1plus5")
-        if let lifeText = label1.text {
-            let components = lifeText.components(separatedBy: ":")
-            if components.count == 2, let currentLife = Int(components[1].trimmingCharacters(in: .whitespaces)) {
-                let newLife = currentLife + 5
-                label1.text = "Player 1 life: \(newLife)"
-            }
-        }
- }
-    @IBAction func player1minus1(_ sender: Any) {
-        print("player1minus1")
-        if let lifeText = label1.text {
-            let components = lifeText.components(separatedBy: ":")
-            if components.count == 2, let currentLife = Int(components[1].trimmingCharacters(in: .whitespaces)) {
-                let newLife = currentLife - 1
-                label1.text = "Player 1 life: \(newLife)"
-            }
-        }
- }
-    @IBAction func player1minus5(_ sender: Any) {
-        print("player1minus5")
-        if let lifeText = label1.text {
-            let components = lifeText.components(separatedBy: ":")
-            if components.count == 2, let currentLife = Int(components[1].trimmingCharacters(in: .whitespaces)) {
-                let newLife = currentLife - 5
-                label1.text = "Player 1 life: \(newLife)"
-            }
-        }
- }
-    
-    @IBAction func player2plus1(_ sender: Any) {
-        print("player2plus1")
-        if let lifeText = label2.text {
-            let components = lifeText.components(separatedBy: ":")
-            if components.count == 2, let currentLife = Int(components[1].trimmingCharacters(in: .whitespaces)) {
-                let newLife = currentLife + 1
-                label2.text = "Player 2 life: \(newLife)"
-            }
-        }
-  }
-    @IBAction func player2plus5(_ sender: Any) {
-        print("player2plus5")
-        if let lifeText = label2.text {
-            let components = lifeText.components(separatedBy: ":")
-            if components.count == 2, let currentLife = Int(components[1].trimmingCharacters(in: .whitespaces)) {
-                let newLife = currentLife + 5
-                label2.text = "Player 2 life: \(newLife)"
-            }
-        }
-  }
-    @IBAction func player2minus1(_ sender: Any) {
-        print("player2minus1")
-        if let lifeText = label2.text {
-            let components = lifeText.components(separatedBy: ":")
-            if components.count == 2, let currentLife = Int(components[1].trimmingCharacters(in: .whitespaces)) {
-                let newLife = currentLife - 1
-                label2.text = "Player 2 life: \(newLife)"
-            }
-        }
-  }
-    @IBAction func player2minus5(_ sender: Any) {
-        print("player2minus5")
-        if let lifeText = label2.text {
-            let components = lifeText.components(separatedBy: ":")
-            if components.count == 2, let currentLife = Int(components[1].trimmingCharacters(in: .whitespaces)) {
-                let newLife = currentLife - 5
-                label2.text = "Player 2 life: \(newLife)"
-            }
-        }
-  }
+ 
 }
 
