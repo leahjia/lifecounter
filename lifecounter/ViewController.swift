0b601bac8b86281885aa133100db877f31b0f62d
Leah Jia <99011487+leahjia@users.noreply.github.com>
2024-04-22 16:38:35 -0700

    stuff in table view

--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -14,59 +14,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
 
-    var strings: [String] = ["Player 1", "Player 2", "Player 3", "Player 4"]
+    var players: [String] = ["Player 1", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6", "Player 7", "Player 8"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 //        view.backgroundColor = .black
 
         tableView.dataSource = self
-    }
-
-    private func parseLifeCount(from label: UILabel) -> Int? {
-        guard let lifeText = label.text else { return nil }
-        let components = lifeText.components(separatedBy: ":")
-        guard components.count == 2 else { return nil }
-        return Int(components[1].trimmingCharacters(in: .whitespaces))
-    }
-    
-    private func updateLife(label: UILabel, change: Int) {
-        if let currentLife = parseLifeCount(from: label) {
-            let newLife = currentLife + change
-            label.text = "Player \(label == label1 ? "1" : "2") life: \(newLife)"
-            if newLife <= 0 {
-                loser.text = "Player \(label == label1 ? "1" : "2") LOSES!"
-            }
-        }
-    }
-
-    @IBAction func player1plus1(_ sender: Any) {
-        updateLife(label: label1, change: 1)
-    }
-
-    @IBAction func player1plus5(_ sender: Any) {
-        let plusAmount = Int(changeNum1.text!) ?? 0
-        updateLife(label: label1, change: plusAmount)
-    }
-
-    @IBAction func player1minus1(_ sender: Any) {
-        updateLife(label: label1, change: -1)
-    }
-
-    @IBAction func player1minus5(_ sender: Any) {
-        let minusAmount = Int(changeNum1.text!) ?? 0
-        updateLife(label: label1, change: -minusAmount)
+        tableView.delegate = self
+        tableView.register(playerCellTableViewCell.nib(), forCellReuseIdentifier: playerCellTableViewCell.identifier)
     }
 }
 
-extension ViewController: UITableViewDataSource {
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return strings.count
+        return players.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = strings[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: playerCellTableViewCell.identifier, for: indexPath) as! playerCellTableViewCell
+        cell.playerLabel.text = players[indexPath.row]
         return cell
     }
 }
---
+++ lifecounter/playerCellTableViewCell.swift
@@ -0,0 +1,64 @@
+//
+//  playerCellTableViewCell.swift
+//  lifecounter
+//
+//  Created by Leah on 4/22/24.
+//
+
+import UIKit
+
+class playerCellTableViewCell: UITableViewCell {
+    
+    static let identifier = "playerCellTableViewCell"
+
+    static func nib() -> UINib {
+        return UINib(nibName: "playerCellTableViewCell", bundle: nil)
+    }
+    
+    @IBOutlet weak var playerLabel: UILabel!
+    @IBOutlet weak var changeNum: UITextField!
+
+    @IBAction func minusOne(_ sender: Any) {
+        updateLife(label: playerLabel, change: -1)
+    }
+    
+    @IBAction func plusOne(_ sender: Any) {
+        updateLife(label: playerLabel, change: 1)
+    }
+    
+    @IBAction func minusNum(_ sender: Any) {
+        updateLife(label: playerLabel, change: Int(changeNum.text!) ?? 0)
+    }
+    
+    @IBAction func plusNum(_ sender: Any) {
+        let num = Int(changeNum.text!) ?? 0
+        updateLife(label: playerLabel, change: -num)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        selectionStyle = .none
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    private func parseLifeCount(from label: UILabel) -> Int? {
+        guard let lifeText = label.text else { return nil }
+        let components = lifeText.components(separatedBy: ":")
+        guard components.count == 2 else { return nil }
+        return Int(components[1].trimmingCharacters(in: .whitespaces))
+    }
+    
+    private func updateLife(label: UILabel, change: Int) {
+        if let currentLife = parseLifeCount(from: label) {
+            let newLife = currentLife + change
+            label.text = "Player \(label == playerLabel ? "1" : "2") life: \(newLife)"
+        }
+    }
+
+}
