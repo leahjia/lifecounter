f4bcc1c25cea7f306332e54680a0e0950e2a39cb
Leah Jia <99011487+leahjia@users.noreply.github.com>
2024-04-22 17:26:38 -0700

    rm loser

--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var changeNum1: UITextField!
     @IBOutlet weak var loser: UILabel!
 
+    @IBOutlet weak var history: UIButton!
+    @IBOutlet weak var addPlayer: UIButton!
     @IBOutlet weak var tableView: UITableView!
 
     var players: [String] = ["Player 1 life: 20", "Player 2 life: 20", "Player 3 life: 20", "Player 4 life: 20", "Player 5 life: 20", "Player 6 life: 20", "Player 7 life: 20", "Player 8 life: 20"]
